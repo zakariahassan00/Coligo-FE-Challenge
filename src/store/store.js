05f484af8604b143ba26fa3eac6d9374456d2d3c
Zakaria Hassan <zakariahassan00@hotmail.com>
2018-11-03 03:31:47 +0200

    removed Redux Extentsion activation code from createStore

--- src/store/store.js
+++ src/store/store.js
@@ -1,19 +1,14 @@
-import { createStore, compose, applyMiddleware } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../reducers";
 import thunk from "redux-thunk";
 
-// const initialState = {
-
-// }
+const initialState = {};
 const middleware = [thunk];
 
 const store = createStore(
   rootReducer,
-  compose(
-    applyMiddleware(...middleware),
-    // activating the redux dev tools
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  initialState,
+  applyMiddleware(...middleware)
 );
 
 export default store;
